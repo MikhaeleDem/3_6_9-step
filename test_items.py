62b3f6cc427d620bcf1a5f1e2fa9e9ad57615c43
Mikhaele <m.dannicov@mail.ru>
2021-05-17 23:25:14 +0300

    Создал отдельный репозиторий для шага
    Signed-off-by: Mikhaele <m.dannicov@mail.ru>

--- test_items.py
+++ test_items.py
@@ -1,10 +1,3 @@
-import time
-import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_should_see_login_link(browser):
